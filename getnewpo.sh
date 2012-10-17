c35d02782c1558c7ae0565254347167c7793b09a
James Michael DuPont <JamesMikeDuPont@gmail.com>
2012-10-17 22:45:05 +0200

    helper scripts

---
+++ getnewpo.sh
@@ -0,0 +1,3 @@
+git add */*/*/*.po */*/*.po */*.po *.po; 
+git commit -m "first po files from the verbatim converter" -a; 
+git push mdupont
---
+++ getnewpo2.sh
@@ -0,0 +1,3 @@
+git add */*/*/*.lang.po */*/*.lang.po */*.lang.po *.lang.po; 
+git commit -m "first po files from the verbatim converter" -a; 
+git push mdupont
